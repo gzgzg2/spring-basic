3cb59e5813a4777649516037b876cbf9b43d768d
uram <gzgzg2@naver.com>
2022-01-20 21:35:15 +0900

    [inflearn] 스프링 핵심 원리
    - 회원 도메인 설계
    - 회원 도메인 개발
    - 회원 도메인 실행과 테스트

---
+++ spring-boot/src/main/java/com/spring/ram/springboot/HoloManRunner.java
@@ -0,0 +1,13 @@
+package com.spring.ram.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class HoloManRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
